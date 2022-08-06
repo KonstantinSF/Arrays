625977afec51c3624b0906cd511c611144c5c3b5
KonstantinSF <kosmos008@gmail.com>
2022-08-06 21:59:58 +0300

    3var Unic_Random done

--- Unique Random/Main.cpp
+++ Unique Random/Main.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
 using namespace std;
+//#define UNIQUE_RANDOM
+//#define UNIQUE_RANDOM1
+#define UNIQUE_RANDOM2
 void main()
 {
 	setlocale(LC_ALL, "");
 	const int N = 10;
 	int arr[N]={};
+#ifdef UNIQUE_RANDOM
 	for (int i = 0; i < N; i++)
 	{
 		arr[i] = rand() % 10;
@@ -18,6 +22,42 @@ void main()
 			}
 		}
 	}
+#endif
+#ifdef UNIQUE_RANDOM1
+	srand(time(NULL));
+	for (int i = 0; i < N; i++)
+	{
+		arr[i] = rand() % N;
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[i] == arr[j])
+			{
+				i--;
+				break;
+			}
+		}
+	}
+#endif
+#ifdef UNIQUE_RANDOM2
+	srand(time(NULL));
+	for (int i = 0; i < N; i++)
+	{
+		bool unique;
+		do
+		{
+			unique = true;
+			arr[i] = rand() % N;
+			for (int j = 0; j < i; j++)
+			{
+				if (arr[i] == arr[j])
+				{
+					unique = false;
+					break;
+				}
+			}
+		} while (!unique);
+	}
+#endif
 	for (int i = 0; i < N; i++) cout << arr[i] << "\t"; 
 	cout << endl;
 }
