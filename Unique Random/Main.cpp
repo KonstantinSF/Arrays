5ef5096d6c490e3ab4c1aabe29c9dcafa35b8c5e
KonstantinSF <kosmos008@gmail.com>
2022-08-06 20:56:31 +0300

    unic random(my ver) done

--- Unique Random/Main.cpp
+++ Unique Random/Main.cpp
@@ -4,13 +4,19 @@ void main()
 {
 	setlocale(LC_ALL, "");
 	const int N = 10;
-	int arr[N];
+	int arr[N]={};
 	for (int i = 0; i < N; i++)
 	{
-		do
-		{
-			if (arr[i] == arr[!i])arr[i] = rand() % 11;
-		} while (arr[i] != arr[!i]);
+		arr[i] = rand() % 10;
+		for (int j=i; j>=0; j--)
+		{ 
+			if (i == j)continue;
+			while (arr[j] == arr[i]&&i!=j)
+			{
+				arr[i] = rand() % 10;
+				j = i;
+			}
+		}
 	}
 	for (int i = 0; i < N; i++) cout << arr[i] << "\t"; 
 	cout << endl;
